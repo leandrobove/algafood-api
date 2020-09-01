0daa953c14ad42f455d8d86e403c7bd56d245537
Leandro Bove <leandrobove@hotmail.com>
2020-09-01 12:53:25 -0300

    Implementação de atualização parcial de Restaurante com PATCH

--- src/main/java/com/github/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/github/algafood/api/controller/RestauranteController.java
@@ -1,13 +1,17 @@
 package com.github.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.algafood.domain.exception.EntidadeEmUsoException;
 import com.github.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.github.algafood.domain.model.Restaurante;
@@ -86,12 +91,44 @@ public class RestauranteController {
 			cadastroRestauranteService.deletar(id);
 
 			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-			
+
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		} catch (EntidadeEmUsoException e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
+	}
+
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long id, @RequestBody Map<String, Object> mapCampos) {
+		Restaurante restauranteAtual = restauranteRepository.buscar(id);
+
+		if (restauranteAtual == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+
+		// lambda expression
+		merge(mapCampos, restauranteAtual);
+
+		atualizar(id, restauranteAtual);
+
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+
+	private void merge(Map<String, Object> mapCamposOrigem, Restaurante restauranteDestino) {
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		Restaurante restauranteOrigem = objectMapper.convertValue(mapCamposOrigem, Restaurante.class);
+		
+		mapCamposOrigem.forEach((chave, valor) -> {
+			Field field = ReflectionUtils.findField(Restaurante.class, chave);
+			field.setAccessible(true);
+			
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
+		});
 	}
 
 }
