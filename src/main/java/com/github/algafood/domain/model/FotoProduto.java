7d7eed0b36eaa5d7cce6b68e5acecab7b20ac433
Leandro Bove <leandrobove@hotmail.com>
2021-11-16 18:03:50 -0300

    Mapeando entidade FotoProduto com relacionamento One-to-One unidirecional

---
+++ src/main/java/com/github/algafood/domain/model/FotoProduto.java
@@ -0,0 +1,36 @@
+package com.github.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+@Entity
+public class FotoProduto {
+
+	@Id
+	@EqualsAndHashCode.Include
+	@Column(name = "produto_id")
+	private Long id;
+
+	private String nomeArquivo;
+
+	private String descricao;
+
+	private String contentType;
+
+	private Long tamanho;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@MapsId
+	private Produto produto;
+
+}
