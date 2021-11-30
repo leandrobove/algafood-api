2931cf1f41b31a4323aa9703baa1f6d4e82ddadf
Leandro Bove <leandrobove@hotmail.com>
2021-11-30 15:08:47 -0300

    Disparar evento após o commit na confirmação de Pedido

--- src/main/java/com/github/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/github/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -1,8 +1,8 @@
 package com.github.algafood.domain.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.github.algafood.domain.event.PedidoConfirmadoEvent;
 import com.github.algafood.domain.model.Pedido;
@@ -11,24 +11,25 @@ import com.github.algafood.domain.service.EnvioEmailService.Mensagem;
 
 @Component
 public class NotificacaoClientePedidoConfirmadoListener {
-	
+
 	@Autowired
 	private EnvioEmailService emailService;
-	
-	@EventListener
+
+	// @EventListener
+	@TransactionalEventListener // executado somente depois do commit
 	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
-		
+
 		Pedido pedido = event.getPedido();
-		
+
 		Mensagem mensagem = Mensagem.builder()
 				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
 				.corpo("pedido-confirmado.html")
 				.destinatario(pedido.getCliente().getEmail())
 				.variavel("pedido", pedido)
 				.build();
-		
+
 		emailService.enviar(mensagem);
-		
+
 	}
 
 }
