fc9689a962bcdb2dbee4502d77000e0bd4f5c5f0
Leandro Bove <leandrobove@hotmail.com>
2021-12-07 16:20:18 -0300

    Adicionando configurações de informações da documentação da API

--- src/main/java/com/github/algafood/domain/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/github/algafood/domain/core/openapi/SpringFoxConfig.java
@@ -3,7 +3,11 @@ package com.github.algafood.domain.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -14,7 +18,18 @@ public class SpringFoxConfig {
 	public Docket configureApiDocket() {
 		return new Docket(DocumentationType.OAS_30)
 				.select()
-				.apis(RequestHandlerSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.github.algafood.api"))
+				.paths(PathSelectors.any())
+				.build()
+					.apiInfo(this.adicionarInformacoesDaApi());
+	}
+	
+	public ApiInfo adicionarInformacoesDaApi() {
+		return new ApiInfoBuilder()
+				.title("Algafood RestAPI")
+				.description("API aberta para clientes e restaurantes")
+				.version("1")
+				.contact(new Contact("Algaworks", "https://algaworks.com", "contact@algaworks.com"))
 				.build();
 	}
 	
