a20c7a00021814a63bdd8a4e08f4d6ad1f3d74a0
Leandro Bove <leandrobove@hotmail.com>
2022-05-30 14:38:52 -0300

    Adicionando log para exceptions não tratadas

--- src/main/java/com/github/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/github/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -33,6 +33,9 @@ import com.github.algafood.domain.exception.EntidadeEmUsoException;
 import com.github.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.github.algafood.domain.exception.NegocioException;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -50,7 +53,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		ProblemType problemType = ProblemType.ERRO_DE_SISTEMA;
 		String detail = MSG_ERRO_GENERICA_USUARIO_FINAL;
 
-		ex.printStackTrace();
+		log.error(ex.getMessage(), ex);
 
 		Problem problem = this.createProblemBuilder(status, problemType, detail).userMessage(detail).build();
 
