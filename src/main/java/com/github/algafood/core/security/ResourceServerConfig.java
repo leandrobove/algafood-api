6709c8849db25751259629eb021b6755df266d4c
Leandro Bove <leandrobove@hotmail.com>
2022-09-02 10:19:20 -0300

    Implementa um Converver no ResourceServer para ler e converter as authorities do JWT

--- src/main/java/com/github/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/github/algafood/core/security/ResourceServerConfig.java
@@ -1,11 +1,21 @@
 package com.github.algafood.core.security;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +27,11 @@ public class ResourceServerConfig {
 	public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			//.antMatchers("/oauth2/**").authenticated()
-			.anyRequest().authenticated()
+			.antMatchers(HttpMethod.POST, "/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
+			.antMatchers(HttpMethod.PUT, "/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
+			.antMatchers(HttpMethod.DELETE, "/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
+			.antMatchers(HttpMethod.GET, "/cozinhas/**").authenticated()
+			.anyRequest().denyAll()
 		.and()
 			.cors()
 		.and()
@@ -25,11 +39,37 @@ public class ResourceServerConfig {
 			//.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		//.and()
 			.oauth2ResourceServer()
-				.jwt();
+				.jwt()
+				.jwtAuthenticationConverter(this.jwtAuthenticationConverter());
 		
 		http.formLogin(Customizer.withDefaults());
 		
 		return http.build();
+	}
+	
+	/*
+	 * Pega os authorities dentro do token JWT e converte em uma Collection de GrantedAuthority
+	 */
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+		
+		converter.setJwtGrantedAuthoritiesConverter((jwt) -> {
+			List<String> authorities = jwt.getClaimAsStringList("authorities");
+			
+			if(authorities == null) {
+				return Collections.emptyList();
+			}
+			
+			JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+			Collection<GrantedAuthority> grantedAuthorities = authoritiesConverter.convert(jwt);
+			
+			grantedAuthorities.addAll(authorities.stream().map(SimpleGrantedAuthority::new)
+					.collect(Collectors.toList()));
+			
+			return grantedAuthorities;
+		});
+		
+		return converter;
 	}
 
 }
