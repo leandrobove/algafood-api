68596aa44d6749087fe2cac98c16324b4eb20f11
Leandro Bove <leandrobove@hotmail.com>
2022-09-04 17:03:45 -0300

    Restringindo acesso dos endpoints por escopos do OAuth2

--- src/main/java/com/github/algafood/core/security/CheckSecurity.java
+++ src/main/java/com/github/algafood/core/security/CheckSecurity.java
@@ -11,13 +11,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 public @interface CheckSecurity {
 
 	public @interface Cozinhas {
-		@PreAuthorize("isAuthenticated()")
+		@PreAuthorize("isAuthenticated() and hasAuthority('SCOPE_READ')")
 		@Retention(RUNTIME)
 		@Target(METHOD)
 		public @interface PodeConsultar {
 		}
 
-		@PreAuthorize("hasAuthority('EDITAR_COZINHAS')")
+		@PreAuthorize("hasAuthority('SCOPE_WRITE') and hasAuthority('EDITAR_COZINHAS')")
 		@Retention(RUNTIME)
 		@Target(METHOD)
 		public @interface PodeEditar {
