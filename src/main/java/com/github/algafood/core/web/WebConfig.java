51b16f9e0040954b41fdeb2293a7ca17995b9ae9
Leandro Bove <leandrobove@hotmail.com>
2022-09-09 14:33:43 -0300

    Remove configuração antiga do CORS

--- src/main/java/com/github/algafood/core/web/WebConfig.java
+++ src/main/java/com/github/algafood/core/web/WebConfig.java
@@ -5,17 +5,16 @@ import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
+	/*@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		//Habilita o CORS globalmente para todos os controllers
 		registry.addMapping("/**").allowedMethods("*");
-	}
+	}*/
 	
 	@Bean
 	public Filter shallowEtagHeaderFilter() {
