bf08558222b85811e423220c4b6526acdbe7008b
Leandro Bove <leandrobove@hotmail.com>
2022-09-14 13:02:09 -0300

    Adicionando status de resposta na documentação de CidadeController

--- src/main/java/com/github/algafood/api/controller/CidadeController.java
+++ src/main/java/com/github/algafood/api/controller/CidadeController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.github.algafood.api.assembler.CidadeModelAssembler;
 import com.github.algafood.api.assembler.input.CidadeInputDisassembler;
 import com.github.algafood.api.dto.CidadeModel;
 import com.github.algafood.api.dto.input.CidadeInput;
+import com.github.algafood.api.exceptionhandler.Problem;
 import com.github.algafood.core.security.CheckSecurity;
 import com.github.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.github.algafood.domain.exception.NegocioException;
@@ -32,6 +34,11 @@ import com.github.algafood.domain.service.CadastroCidadeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 
 @Api(tags = "Cidades")
 
@@ -63,6 +70,14 @@ public class CidadeController {
 
 	@CheckSecurity.Cidades.PodeConsultar
 	@ApiOperation(value = "Busca uma cidade por ID")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "400", description = "ID da Cidade é inválido", 
+				content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, 
+				schema = @Schema(implementation = Problem.class))),
+		@ApiResponse(responseCode = "404", description = "Cidade não encontrada", 
+				content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, 
+				schema = @Schema(implementation = Problem.class)))	
+	})
 	@GetMapping(value = "/{cidadeId}")
 	public CidadeModel buscar(@ApiParam(value = "ID de uma cidade") @PathVariable Long cidadeId) {
 
@@ -93,6 +108,11 @@ public class CidadeController {
 
 	@CheckSecurity.Cidades.PodeEditar
 	@ApiOperation(value = "Atualiza uma cidade por ID")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "404", description = "Cidade não encontrada", 
+				content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, 
+				schema = @Schema(implementation = Problem.class)))	
+	})
 	@PutMapping(value = "/{cidadeId}")
 	public CidadeModel atualizar(@ApiParam(value = "ID de uma cidade") @PathVariable Long cidadeId,
 			@RequestBody @Valid CidadeInput cidadeDTOInput) {
@@ -110,6 +130,11 @@ public class CidadeController {
 
 	@CheckSecurity.Cidades.PodeEditar
 	@ApiOperation(value = "Exclui uma cidade por ID")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "404", description = "Cidade não encontrada", 
+				content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, 
+				schema = @Schema(implementation = Problem.class)))	
+	})
 	@DeleteMapping(value = "/{cidadeId}")
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void deletar(@ApiParam(value = "ID de uma cidade") @PathVariable Long cidadeId) {
